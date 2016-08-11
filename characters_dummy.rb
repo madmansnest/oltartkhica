7358aa7991ea708f142abe1c536b334e532fa361
Demian Terentev <demian@infusiastic.me>
2016-08-11 16:21:36 +0800

    Added modifiers for all Pattern and Stems with Framed Relation.
    Fixed small mistakes in Aultartkʰiupš.
    Added Bias codes to Aultartkʰiupš.
    Added placeholder bias characters.
    Updated samples.

--- characters_dummy.rb
+++ characters_dummy.rb
@@ -1,23 +1,23 @@
 number = 92 # Characters per modifier
 #startchar, lig, startnum1, startnum2, startchar, lig, startchar, lig
 char = 0xe0c7
-lig = 0xe267
-startnum1 = 66181
-startnum2 = 1218
+lig = 0xe270
+startnum1 = 67009
+startnum2 = 2056
 template = %q{StartChar: uni%X%X.liga
 Encoding: %d -1 %d
 Width: 1000
-Flags: HW
-HStem: -15 819<494.632 494.632>
-VStem: 149.632 655<605 804>
+Flags: W
+HStem: 801 165<748 752>
+VStem: 587 257<888 965>
 LayerCount: 2
 Fore
 SplineSet
-412.26171875 335.96875 m 5
- 581.747070312 425.334960938 l 5
- 515.631835938 513 l 5
- 411.884765625 454.025390625 l 5
- 412.26171875 335.96875 l 5
+844 888 m 1
+ 752 966 l 1
+ 587 965 l 1
+ 748 801 l 1
+ 844 888 l 1
 EndSplineSet
 Ligature2: "PatternStemRelation" uni%X uni%X
 EndChar

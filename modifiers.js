fce2c040ca10c748cf6176961a92b023d6426113
Demian Terentev <demian@infusiastic.me>
2016-09-07 09:33:21 +0800

    Fixed IME bugs.

--- modifiers.js
+++ modifiers.js
@@ -13,7 +13,7 @@ var ligatures_tb_start = 0xf209c;
 
 function assemble_last(s) {
   cp = punycode.ucs2.decode(s);
-  console.log(cp);
+  // console.log(cp);
   // lc is last character
   // slc is second last character
   lc = cp[cp.length-1];
@@ -21,11 +21,12 @@ function assemble_last(s) {
     slc = cp[cp.length-2];
     if (slc==0xe185&&lc>=0x19b&&lc<=0xe1ab) { // Laterally reversed placeholder?
       // Simple replacement with laterally reversed version
-      return s.substring(0,s.length-2) + String.fromCodePoint(lc+186);
+      return punycode.ucs2.encode($.merge(cp.slice(0,-2), [lc+186]));
     }
     if (slc==0xe184||slc==0xe186) { // Placeholder?
       // Simple removal of placeholder character
-      return s.substring(0,s.length-2) + s.charAt(s.length-1);
+      cp.splice(-2,1);
+      return punycode.ucs2.encode(cp);
     }
     else if (lc==0xe253||lc==0xe254) { // Bias with diacritic?
       // Bias diacritic characters are input together with the corresponding extension. Therefore, end of string without the diacritic should be replaced if necessary, and then the diacritic added back.
@@ -33,27 +34,30 @@ function assemble_last(s) {
       return assemble_last(s.substring(0,s.length-1)) + bias_diacritic;
     }
     else if ((slc>=0xe000&&slc<=0xe05f)&&(lc>=0xe18a&&lc<=0xe19a)) { // Primary with top?
-      return s.substring(0,s.length-2) + String.fromCodePoint((lc-top_start)*96+(slc-primary_start)+primary_ligatures_top_start);
+      console.log("p+top", cp);
+      return punycode.ucs2.encode($.merge(cp.slice(0,-2), [(lc-top_start)*96+(slc-primary_start)+primary_ligatures_top_start]));
     }
     else if ((slc>=0xe000&&slc<=0xe05f)&&(lc>=0xe1ac&&lc<=0xe1c2)) { // Primary with bottom?
-      return s.substring(0,s.length-2) + String.fromCodePoint((lc-bottom_start)*96+(slc-primary_start)+primary_ligatures_bottom_start);
+      console.log("p+bottom", cp);
+      return punycode.ucs2.encode($.merge(cp.slice(0,-2), [(lc-bottom_start)*96+(slc-primary_start)+primary_ligatures_bottom_start]));
     }
     else if ((slc>=0xe127&&slc<=0xe182)&&(lc>=0xe19b&&lc<=0xe1ab)) { // Consonantal with top?
-      return s.substring(0,s.length-2) + String.fromCodePoint((lc-top_start)*96+(slc-primary_start)+primary_ligatures_top_start);
+      console.log("c+top", cp);
+      return punycode.ucs2.encode($.merge(cp.slice(0,-2), [(lc-c_top_start)*96+(slc-consonantal_start)+consonantal_ligatures_top_start]));
     }
-    else if ((slc>=0xe127&&slc<=0xe182)&&(lc>=0xe1ac&&lc<=0xe1cc)) { // Consonantal with bottom?
-      return s.substring(0,s.length-2) + String.fromCodePoint((lc-bottom_start)*92+(slc-consonantal_start)+consonantal_ligatures_bottom_start);
+    else if ((slc>=0xe127&&slc<=0xe182)&&(lc>=0xe1ac&&lc<=0xe1cb)) { // Consonantal with bottom?
+      console.log("p+bottom", cp);
+      return punycode.ucs2.encode($.merge(cp.slice(0,-2), [(lc-bottom_start)*92+(slc-consonantal_start)+consonantal_ligatures_bottom_start]));
     }
-    else if ((slc>=0xf0000&&slc<=0xf065f)&&(lc>=0xe1ac&&lc<=0xe1cc)) { // Primary with top + bottom?
-      var new_cp = cp.slice(0,cp.length-2);
-      new_cp.push((lc-bottom_start)*96*17+(slc-primary_ligatures_top_start)+ligatures_tb_start);
-      return punycode.ucs2.encode(new_cp);
+    else if ((slc>=0xf0000&&slc<=0xf065f)&&(lc>=0xe1ac&&lc<=0xe1cb)) { // Primary with top + bottom?
+      console.log("ptop+bottom", cp);
+      return punycode.ucs2.encode($.merge(cp.slice(0,-2), [(lc-bottom_start)*96*17+(slc-primary_ligatures_top_start)+ligatures_tb_start]));
     }
+  }
+  return s;
     // else if ((slc>=0xf0660&&slc<=0xf0eff)&&(lc>=0xe18a&&lc<=0xe19a)) { // Primary with bottom + top? — REQUIRES MORE MAGIC
     //   var new_cp = cp.slice(0,cp.length-2);
     //   new_cp.push((lc-bottom_start)*96*17+(slc-primary_ligatures_top_start)+ligatures_tb_start);
     //   return punycode.ucs2.encode(new_cp);
     // }
-  }
-  return s; 
 }
